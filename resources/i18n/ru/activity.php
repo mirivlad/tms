cbada09f6be12013a3ca0b82ee93830149119f98
Mirivlad <mirivlad@gmail.com>
2026-09-22 16:27:15 +0800

    feat(activity): add activity history foundation

---
+++ resources/i18n/ru/activity.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'activity.title' => 'История',
+    'activity.task_title' => 'История: {task}',
+    'activity.project_title' => 'История изменений',
+    'activity.help' => 'Хронология важных изменений задачи и проекта, доступных в текущем контексте прав.',
+    'activity.empty' => 'История пока пуста.',
+    'activity.back_to_task' => 'К задаче',
+    'activity.by' => 'изменил: {user}',
+    'activity.event.task.created' => 'Задача создана',
+    'activity.event.task.updated' => 'Задача изменена',
+    'activity.event.project.created' => 'Проект создан',
+    'activity.event.project.updated' => 'Проект изменён',
+    'activity.field.title' => 'Название',
+    'activity.field.name' => 'Название',
+    'activity.field.description' => 'Описание',
+    'activity.field.status' => 'Статус',
+    'activity.field.type' => 'Тип',
+    'activity.field.priority' => 'Приоритет',
+    'activity.field.customer' => 'Заказчик',
+    'activity.field.project' => 'Проект',
+    'activity.field.assignee' => 'Ответственный',
+    'activity.field.deadline' => 'Срок',
+    'activity.field.lifecycle_status' => 'Состояние проекта',
+    'activity.field.owner' => 'Владелец',
+    'activity.value.changed' => 'Изменено',
+];
