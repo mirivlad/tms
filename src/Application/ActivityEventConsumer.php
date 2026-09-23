4510958d50b0a05a1bd01bdd0c9a7a50c69d8e5f
Mirivlad <mirivlad@gmail.com>
2026-09-23 05:13:07 -0700

    feat: consume domain events into activity history

---
+++ src/Application/ActivityEventConsumer.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tms\Application;
+
+use Tms\Domain\Activity\ActivityRepository;
+use Tms\Domain\Event\DomainEvent;
+use Tms\Domain\Event\DomainEventConsumer;
+
+final readonly class ActivityEventConsumer implements DomainEventConsumer
+{
+    public function __construct(private ActivityRepository $activity)
+    {
+    }
+
+    public function consume(DomainEvent $event): void
+    {
+        if (!str_starts_with($event->type, 'task.') && !str_starts_with($event->type, 'project.')) {
+            return;
+        }
+        $this->activity->recordDomainEvent($event);
+    }
+}
